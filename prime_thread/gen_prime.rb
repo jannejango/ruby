a00bf1ebdde6e825362bf5d4f77da72429260ee6
jannejango <jannejango@users.noreply.github.com>
2014-02-17 00:10:05 +0900

    judge prime, many requests

--- prime_thread/gen_prime.rb
+++ prime_thread/gen_prime.rb
@@ -1,10 +1,28 @@
 require 'logger'
 require 'socket'
 
-# 乱数を生成して、判定プロセスに送りつける
+def rand_num
+	return Random.rand(1_000_000..1_000_000_000)
+end
 
-TCPSocket.open('localhost', 4444) do |sock|
-	num = Random.rand(1_000_000..1_000_000_000)
-	sock.write(num)
+def gen_rand_nums
+	log = Logger.new(STDOUT)
+
+	str = '['
+	str += rand_num.to_s
+	9.times do
+		str += ',' + rand_num.to_s
+	end
+	str += ']'
+	log.debug "str: #{str}"
+	return str
+end
+
+# 乱数を生成して、判定プロセスに送りつける
+loop do
+	TCPSocket.open('localhost', 4444) do |sock|
+		sock.write(gen_rand_nums)
+		sleep 3
+	end
 end
 
--- prime_thread/judge_prime.rb
+++ prime_thread/judge_prime.rb
@@ -13,40 +13,42 @@ def prime?(num)
 
 	# 順番に割り切れるか試す
 	(2..half).each do |i|
-		# １秒ごとにドットを表示
-		if count >= 100_000
-			now = Time.now
-			diff = now - start
-			if diff >= 1
-				# log.debug diff
-				print '.'
-				start = now
-			end
-			count = 0
-		end
 
 		# 割り切れたら素数ではない
 		if num % i == 0
-			log.debug "#{num} / #{i} = #{num/i}"
+			# log.debug "#{num} / #{i} = #{num/i}"
 			return false
 		end
 		count += 1
 	end
 
 	# 全て割り切れなかったら素数
-	puts
+	# puts
 	log.debug "#{num} is prime number."
 	return true
 end
 
 def main
 	queue = Queue.new
+	log = Logger.new(STDOUT)
+	count = 0
+	total_time = 0.0
+	ave_time = 0.0
 
 	# workerスレッドを作成。キューに要求が届き次第、素数判定を行う。
-	workers = 3.times do |t|
+	workers = 10.times do |t|
 		Thread.fork do
 			while req = queue.deq
-				puts "worker#{t}: #{prime?(req)}"
+				start = Time.now
+				res = prime?(req)
+				finish = Time.now
+				# puts "worker#{t}: #{res}"
+
+				count += 1
+				total_time += finish - start
+				ave_time = total_time / count
+				# puts "worker#{t}: count: #{count}"
+				# puts "average time: #{ave_time}"
 			end
 		end
 	end
@@ -56,7 +58,13 @@ def main
 		loop do
 			s = sock.accept
 			str = s.read
-			queue.enq(str.to_i)
+			arr = eval("#{str}")
+			arr.each do |num|
+				queue.enq(num)
+			end
+			log.debug "current queue size: #{queue.size}"
+			log.debug "count: #{count}"
+			log.debug "average processing time: #{ave_time}"
 		end
 	end
 end
